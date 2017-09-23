b33be9f36e65003cf89d8d8c08b7f002891820c6
Peter Benvenuto <peterbenvenuto90@gmail.com>
2017-09-23 23:06:56 +0000

    Adds conditionals exercises

---
+++ logical-operators.js
@@ -0,0 +1,18 @@
+var isCitizen = true;
+var age = 19
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+var temperature = 45;
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+var raining = false;
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
